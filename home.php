c971da9183aecbf1874d64c4633ea62e263b8a52
Jay-ar Dela Rama <jayardelarama27@gmail.com>
2021-12-27 23:03:40 +0800

    deleted home html

--- home.php
+++ home.php
@@ -1,30 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="allstyle.css">
-    <title>AAA - Cleaning Products</title>
-</head>
-<body>
-    <div class="cont1">
-        <div class="cont1-logo">
-            <img  src="./img/aaalogo2.png" class="logo" alt="AAA logo" >
-        </div>
-         <div class="cont1-button">
-               <ul class="cont1-ul">
-                    <li><a href="home.php" class="list">HOME</a></li>
-                    <li><a href="about.php" class="list">ABOUT US</a></li>
-                    <li><a href="gallery.php" class="list">SHOP</a></li>
-                    <li><a href="order.php" class="list">ORDER NOW</a></li>
-                    <li><a href="contact.php" class="list">CONTACT</a></li>
-                    <li><input type="search" placeholder="Search Products" class="search-bar"></li>
-                </ul> 
-        </div>
-    </div>
-</body>
-</html> -->
 <?php
 include 'home.html';
 
