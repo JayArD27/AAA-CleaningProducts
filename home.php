1673586fc7411f2245fd194f29016ee1fb68ec8f
Jay-ar Dela Rama <jayardelarama27@gmail.com>
2021-12-27 22:09:16 +0800

    separaed php

--- home.php
+++ home.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="style.css"> -->
     <link rel="stylesheet" href="allstyle.css">
     <title>AAA - Cleaning Products</title>
 </head>
@@ -25,9 +24,9 @@
         </div>
     </div>
 </body>
-</html>
+</html> -->
 <?php
-
+include 'home.html';
 
 
 
--- login.php
+++ login.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="allstyle.css">
-    <title>LOG IN</title>
-</head>
-<body class="login-page">
-    <form method="POST" action="login.php">
-        <div id="main-logindiv">
-            <div class="login">
-                <div class="login-div1">
-                    <input type="text" name="username" placeholder="E-MAIL OR USERNAME" class="login-input">
-                </div>
-                <div class="login-div2">
-                    <input type="password" name="password" placeholder="PASSWORD"  class="login-input">
-                </div>
-                <div class="login-button"><button class="loginbutton"><a href="home.php" class="loginbutton">LOG IN</a></button></div>
-                <div class="signup-button">
-                    <p>Can't Log-in? <a href="">Sign Up Now</a></p>
-                </div>
-            </div>
-        </div>
-    </form>
-</body>
-</html>
+<?php
+include 'login.html';
+?>
