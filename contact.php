98e2bacd93aabb8dd8e2c2d367fd3e0135186a00
Jay-ar Dela Rama <jayardelarama27@gmail.com>
2022-01-04 22:57:51 +0800

    added few changes

--- contact.php
+++ contact.php
@@ -1,9 +1,12 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title></title>
-
-<link rel=stylesheet href="allstyle.css">
-<body>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="allstyle.css">
+    <link rel="icon" href="img/aaalogo2.png">
+    <title>CONTACT US</title>
 </head>
 <body>
 
