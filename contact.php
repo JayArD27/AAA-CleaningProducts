233808c35db2933dcce3788be3120dd46a5f63c5
Jay-ar Dela Rama <jayardelarama27@gmail.com>
2022-01-07 22:46:55 +0800

    modified contact

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="allstyle.css">
+    <link rel="stylesheet" href="contactstyle.css">
     <link rel="icon" href="img/aaalogo2.png">
     <title>CONTACT US</title>
 </head>
