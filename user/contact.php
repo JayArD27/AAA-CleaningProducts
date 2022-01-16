7551c22c2f33f44b39878de5095bedb680fa1ceb
Jay-ar Dela Rama <jayardelarama27@gmail.com>
2022-01-16 18:15:11 +0800

    modified to link in stylesheet

--- user/contact.php
+++ user/contact.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/contactstyle.css">
-    <link rel="icon" href="img/aaalogo2.png">
+    <link rel="stylesheet" href="../style/contactstyle.css">
+    <link rel="icon" href="../img/aaalogo2.png">
     <title>CONTACT US</title>
 </head>
 <body>
 
 <div class="cont1">
         <div class="cont1-logo">
-            <img  src="./img/aaalogo2.png" class="logo" alt="AAA logo" >
+            <img  src="../img/aaalogo2.png" class="logo" alt="AAA logo" >
         </div>
          <div class="cont1-button">
                <ul class="cont1-ul">
